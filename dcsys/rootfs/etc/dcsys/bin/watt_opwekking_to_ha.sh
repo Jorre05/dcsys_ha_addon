14ad7d262ceb9ebf2e81a7efc60729c9e7b028de
Joris <joris@struyve.be>
2024-07-22 10:21:30 +0200

    wip

--- dcsys/rootfs/etc/dcsys/bin/watt_opwekking_to_ha.sh
+++ dcsys/rootfs/etc/dcsys/bin/watt_opwekking_to_ha.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --header "Content-Type: application/json" --request POST --data '{"opwekking": '$1'}'  http://localhost:8123/api/webhook/opwekking_webhook
+curl -s --header "Content-Type: application/json" --request POST --data '{"opwekking": '$1'}'  http://dcsys.stuyve.lan:8123/api/webhook/opwekking_webhook
 
--- dcsys/rootfs/etc/dcsys/bin/watt_to_ha.sh
+++ dcsys/rootfs/etc/dcsys/bin/watt_to_ha.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --header "Content-Type: application/json" --request POST --data '{"vermogen": '$1'}'  http://localhost:8123/api/webhook/vermogen_webhook
+curl -s --header "Content-Type: application/json" --request POST --data '{"vermogen": '$1'}'  http://dcsys.struyve.lan:8123/api/webhook/vermogen_webhook
 
